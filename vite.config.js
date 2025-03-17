d0d47b49b8e2e704d53615dfcd7bd0e8836622ef
ZephyroYz <luisangel17050713@gmail.com>
2025-03-17 02:17:35 -0600

    V2.1

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/sass/app.scss',
-            'resources/js/app.js',
-        ]),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build',  // Este es el directorio de salida
+  }
 });
