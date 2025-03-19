ef132d322a1f36e8740a811fcfe30b9d8acc93c5
ZephyroYz <luisangel17050713@gmail.com>
2025-03-18 21:23:55 -0600

    Cambios en Soporte y en Descarga

--- resources/views/descarga.blade.php
+++ resources/views/descarga.blade.php
@@ -20,7 +20,9 @@
             <!-- Cuadro blanco con fondo color -->
             <div class="container bg-white p-4 rounded shadow">
         <h1>Descarga del Software</h1>
+        <p><strong>Nota:</strong> El navegador puede marcar este archivo como inseguro porque es un archivo .zip que contiene un ejecutable. Puedes proceder con la descarga de manera segura.</p>
         <p>Gracias por tu compra. Descarga el software aquí:</p>
+        
         <a href="{{ asset('descargas/PruebaEduBeaver.zip') }}" class="btn btn-success" download>Descargar</a>
     </div>
 </div>
--- resources/views/soporte.blade.php
+++ resources/views/soporte.blade.php
@@ -10,13 +10,22 @@
         <div>
             <h3>Preguntas Frecuentes</h3>
             <ul>
-                <li><strong>¿Cómo puedo recuperar mi contraseña?</strong> ...</li>
-                <li><strong>¿Cómo reportar un error?</strong> ...</li>
-                <li><strong>¿Cómo puedo cambiar mi dirección de correo electrónico?</strong> ...</li>
-                <li><strong>¿Dónde puedo ver mis facturas?</strong> ...</li>
-                <li><strong>¿Cómo puedo cancelar mi suscripción?</strong> ...</li>
-                <li><strong>¿Qué métodos de pago aceptan?</strong> ...</li>
-                <!-- Agrega más preguntas frecuentes aquí -->
+                <li>
+                    <strong>¿Cómo puedo contactar con soporte?</strong>
+                    <p>Puedes contactarnos enviando un correo a <strong>elisyum1@gmail.com</strong>. Estamos disponibles de lunes a viernes, de 9:00 a 18:00.</p>
+                </li>
+                <li>
+                    <strong>¿Cómo puedo cambiar mi contraseña?</strong>
+                    <p>Para cambiar tu contraseña, ve a la sección de "Perfil" y navega al apartado de "Cambiar contraseña". Introduce tu contraseña actual y la nueva contraseña.</p>
+                </li>
+                <li>
+                    <strong>¿Cómo puedo actualizar mi perfil?</strong>
+                    <p>Para actualizar tu perfil, ve a la sección de configuración de tu cuenta y edita la información que deseas actualizar, (como la foto de perfil, correo, y contraseña). No olvides guardar los cambios.</p>
+                </li>
+                <li>
+                    <strong>¿Cómo puedo eliminar mi cuenta?</strong>
+                    <p>Si deseas eliminar tu cuenta, por favor, envíanos un correo a soporte@ejemplo.com con tu solicitud. Procesaremos la eliminación de tu cuenta en un plazo de 48 horas.</p>
+                </li>
             </ul>
         </div>
 
