85e78cd63ffc67db4528f89fde5a910eb6727a8d
ZephyroYz <luisangel17050713@gmail.com>
2025-03-24 10:26:59 -0600

    EduArcadeV1.8

--- resources/views/descarga.blade.php
+++ resources/views/descarga.blade.php
@@ -19,9 +19,17 @@
 <div class="fondo-inicio">
             <!-- Cuadro blanco con fondo color -->
             <div class="container bg-white p-4 rounded shadow">
-        <h1>Descarga del Software</h1>
-        <p><strong>Nota:</strong> El navegador puede marcar este archivo como inseguro porque es un archivo .zip que contiene un ejecutable. Puedes proceder con la descarga de manera segura.</p>
-        <p>Gracias por tu compra. Descarga el software aquí:</p>
+            <p>
+            ¡Gracias por tu compra! Te presentamos <strong>EduArcade Shooter 2D</strong>, un emocionante juego arcade de disparos en 2D diseñado con la temática única de EduArcade. Sumérgete en una aventura llena de acción y desafíos, donde la habilidad y la estrategia son clave para alcanzar la victoria.
+        </p>
+        <p>
+            Esta es la versión <strong>1.7</strong>, que incluye mejoras significativas y nuevas características para una experiencia de juego aún más inmersiva.
+        </p>
+        <p>
+            <strong> Descarga el software aquí:</strong>
+           
+        </p>
+
         
         <a href="{{ asset('descargas/PruebaEduBeaver.zip') }}" class="btn btn-success" download>Descargar</a>
     </div>
